ff59e4c00099c0b4874ea8dec433fb758288db63
Nathan <natemi1010@gmail.com>
2020-12-02 10:40:48 +0000

    Created input rebinding

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace DapperDino.InputSystemTutorials
+{
+    public class PlayerController : MonoBehaviour
+    {
+        [Header("References")]
+        [SerializeField] private Animator animator = null;
+        [SerializeField] private PlayerInput playerInput = null;
+        [SerializeField] private CharacterController controller = null;
+
+        [Header("Settings")]
+        [SerializeField] private float movementSpeed = 5f;
+
+        private Vector3 inputMovement;
+
+        public PlayerInput PlayerInput => playerInput;
+
+        private void Update()
+        {
+            var finalMovement = inputMovement;
+
+            finalMovement *= movementSpeed * Time.deltaTime;
+
+            controller.Move(finalMovement);
+
+            Vector3 velocity = controller.velocity;
+            velocity.y = 0f;
+
+            if (velocity.magnitude > 0.2f)
+            {
+                transform.rotation = Quaternion.LookRotation(finalMovement);
+            }
+
+            animator.SetBool("IsWalking", velocity.magnitude > 0.2f);
+        }
+
+        public void Move(InputAction.CallbackContext ctx)
+        {
+            var inputValue = ctx.ReadValue<Vector2>();
+            inputMovement = new Vector3(inputValue.x, 0f, inputValue.y);
+        }
+
+        public void Jump(InputAction.CallbackContext ctx)
+        {
+            if (!ctx.performed) { return; }
+
+            animator.SetTrigger("Jump");
+        }
+    }
+}
---
+++ Assets/Scripts/RebindingDisplay.cs
@@ -0,0 +1,49 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace DapperDino.InputSystemTutorials
+{
+    public class RebindingDisplay : MonoBehaviour
+    {
+        [Header("References")]
+        [SerializeField] private InputActionReference actionReference = null;
+        [SerializeField] private PlayerController playerController = null;
+        [SerializeField] private TMP_Text bindingDisplayNameText = null;
+        [SerializeField] private GameObject startRebindObject = null;
+        [SerializeField] private GameObject waitingForInputObject = null;
+
+        private InputActionRebindingExtensions.RebindingOperation rebindingOperation;
+
+        public void StartRebinding()
+        {
+            startRebindObject.SetActive(false);
+            waitingForInputObject.SetActive(true);
+
+            playerController.PlayerInput.SwitchCurrentActionMap("Menu");
+
+            rebindingOperation = actionReference.action.PerformInteractiveRebinding()
+                .WithControlsExcluding("Mouse")
+                .OnMatchWaitForAnother(0.1f)
+                .OnComplete(operation => RebindCompleted())
+                .Start();
+        }
+
+        private void RebindCompleted()
+        {
+            int controlBindingIndex = actionReference.action.GetBindingIndexForControl(actionReference.action.controls[0]);
+
+            bindingDisplayNameText.text = InputControlPath.ToHumanReadableString(
+                actionReference.action.bindings[controlBindingIndex].effectivePath,
+                InputControlPath.HumanReadableStringOptions.OmitDevice);
+
+            rebindingOperation.Dispose();
+            rebindingOperation = null;
+
+            startRebindObject.SetActive(true);
+            waitingForInputObject.SetActive(false);
+
+            playerController.PlayerInput.SwitchCurrentActionMap("Gameplay");
+        }
+    }
+}
